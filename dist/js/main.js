85d711d478318e3edc76c527b9dcc50609340ef8
Brett Marsh <brett.m.coding@gmail.com>
2019-01-10 04:38:04 -0500

    desktop menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,6 +26,6 @@ function toggleMenu() {
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
-    showMenu = true; 
+    showMenu = false; 
   }
 }
